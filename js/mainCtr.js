ebf1104c73ab46e329848a0847ce4ea4bd78ee65
Cody Soultz <cody-soultz@uiowa.edu>
2016-08-18 00:39:46 -0500

    implimented pagination should be all done now but will test later

--- js/mainCtr.js
+++ js/mainCtr.js
@@ -21,7 +21,21 @@ app.controller('mainCtr', function(getterService,$scope) {
 	$scope.reverse = false;
 	$scope.beginAt=0;
 	$scope.itemsPerPage=15;
-	$scope.filteredList="";
+	$scope.filteredList=[{}];
+	$scope.filteredLength=0;
+	$scope.sorting="All";
+	$scope.page=[
+		{page:1,isActive:true,isHidden:false},
+		{page:2,isActive:false,isHidden:false},
+		{page:3,isActive:false,isHidden:false},
+		{page:4,isActive:false,isHidden:false},
+		{page:5,isActive:false,isHidden:false},
+		{page:6,isActive:false,isHidden:false},
+		{page:7,isActive:false,isHidden:false}
+	];
+	$scope.leftVisible=false;
+	$scope.rightVisible=true;
+	var totalPages=0;
 	
 	getterService.async().then(function(response){
 		$scope.data=response.data;
@@ -31,6 +45,7 @@ app.controller('mainCtr', function(getterService,$scope) {
 	});
 	
 	$scope.changeFilter = function (filter) {
+		$scope.sorting=filter;
 		if(filter==="All"){
 			$scope.nameFilter=true;
 			$scope.occupationFilter=true;
@@ -76,33 +91,96 @@ app.controller('mainCtr', function(getterService,$scope) {
 	};
 
 	$scope.next = function(){
-		var holder = $scope.beginAt;
-		$scope.beginAt=$scope.beginAt+$scope.itemsPerPage;
-		if($scope.beginAt>=$scope.data.length){
-			console.log('Something went wrong revert value back to ' + holder);
-			$scope.beginAt=holder;
+		var visiblePages=0;
+		for(i=0;i<$scope.page.length;i++){
+			if($scope.page[i].isActive&&$scope.page[i].page!==totalPages){
+				refreshPagination($scope.page[i].page+1);
+			}
 		}
-		console.log('next was called and the new beginAt is ' + $scope.beginAt);
 	};
 
-	//Still Buggy
 	$scope.previous = function(){
-		if(($scope.beginAt-$scope.itemsPerPage)<0){
-			console.log('Something went wrong');
-			$scope.beginAt=0;
+		for(i=1;i<$scope.page.length;i++){
+			if($scope.page[i].isActive){
+				refreshPagination($scope.page[i].page-1);
+			}
 		}
-		else if($scope.beginAt){
-			console.log('keep at 0');
-			$scope.beginAt=0;
-		}
-		else{
-			console.log('subtract');
-			$scope.beginAt=$scope.beginAt-$scope.itemsPerPage;
-		}
-		console.log('previous was called and the new beginAt is ' + $scope.beginAt);
 	};
 	
 	$scope.$watch('filteredList', function() {
-			console.log('filteredList:', $scope.filteredList.length);
-    });
+		if($scope.READY){
+			$scope.filteredLength=$scope.filteredList.length;
+			refreshPagination(1);
+		}
+	});
+	
+	$scope.pageClicked= function(theClickedPage){
+		console.log("The Clicked Page: " + theClickedPage.page)
+		refreshPagination(theClickedPage.page);
+	};
+	
+	refreshPagination=function(pageNumber){
+		totalPages=Math.floor($scope.filteredLength/$scope.itemsPerPage);
+		$scope.page[6].page=totalPages;
+		$scope.page[1].isHidden=false;
+		$scope.page[2].isHidden=false;
+		$scope.page[3].isHidden=false;
+		$scope.page[4].isHidden=false;
+		$scope.page[5].isHidden=false;
+		$scope.page[6].isHidden=false;
+		switch(totalPages) {
+			case 0:
+			case 1:
+				$scope.page[1].isHidden=true;
+			case 2:
+				$scope.page[2].isHidden=true;
+			case 3:
+				$scope.page[3].isHidden=true;
+			case 4:
+				$scope.page[4].isHidden=true;
+			case 5:
+				$scope.page[5].isHidden=true;
+			case 6:
+				$scope.page[6].isHidden=true;
+			case 7:
+				$scope.rightVisible=false;
+				break;
+			default:
+				$scope.rightVisible=true;
+		}
+		if(pageNumber>4){
+			$scope.leftVisible=true;
+			if((totalPages-pageNumber)<=3){
+				$scope.rightVisible=false;
+				$scope.page[5].page=totalPages-1;
+				$scope.page[4].page=totalPages-2;
+				$scope.page[3].page=totalPages-3;
+				$scope.page[2].page=totalPages-4;
+				$scope.page[1].page=totalPages-5;
+			}
+			else{
+				$scope.page[1].page=pageNumber-2;
+				$scope.page[2].page=pageNumber-1;
+				$scope.page[3].page=pageNumber;
+				$scope.page[4].page=pageNumber+1;
+				$scope.page[5].page=pageNumber+2;
+			}
+		}
+		else{
+			$scope.page[1].page=2;
+			$scope.page[2].page=3;
+			$scope.page[3].page=4;
+			$scope.page[4].page=5;
+			$scope.page[5].page=6;
+		}
+		for(i=0;i<$scope.page.length;i++){
+			if($scope.page[i].page===pageNumber){
+				$scope.page[i].isActive=true;
+			}
+			else{
+				$scope.page[i].isActive=false;
+			}
+		}
+		$scope.beginAt=pageNumber*$scope.itemsPerPage;
+	}
 });
